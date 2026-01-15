fe9094f2e8c33d442cfaf2294c41144f945518d3
somT-oss <somtochukwuuchegbu@gmail.com>
2026-01-15 10:44:25 +0100

    fix: print out image digest

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+set -ex
+
+
 # Log in to ECR
 # We assume the instance role has permissions to run this command
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $(cat /home/ec2-user/image_tag.txt | cut -d'/' -f1)
+# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $(cat /home/ec2-user/image_tag.txt | cut -d'/' -f1)
+
+echo "=== WHO AM I ==="
+whoami
+
+echo "=== CURRENT WORKING DIRECTORY ==="
+pwd
+
+echo "=== LIST CURRENT DIRECTORY ==="
+ls -la
+
+echo "=== TREE (2 levels) ==="
+find . -maxdepth 2 -type f
+
+echo "=== CODEDEPLOY ENV VARS ==="
+env | sort
+
+IMAGE=$(cat image_tag.txt)
+REGISTRY=$(echo "$IMAGE" | awk -F/ '{print $1}')
+
+aws ecr get-login-password --region us-east-1 \
+| docker login --username AWS --password-stdin "$REGISTRY"
 
 ACTIVE_IMAGE=$(cat /home/ec2-user/image_tag.txt)
 
