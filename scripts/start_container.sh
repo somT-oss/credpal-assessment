9595f0ace4f0ddc36301ad4cdb5218ba1b1b69fd
somT-oss <somtochukwuuchegbu@gmail.com>
2026-01-15 13:50:22 +0100

    fix: appspec.yml

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
-set -ex
 
-
-# Log in to ECR
-# We assume the instance role has permissions to run this command
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $(cat /home/ec2-user/image_tag.txt | cut -d'/' -f1)
-
-echo "=== WHO AM I ==="
-whoami
-
-echo "=== CURRENT WORKING DIRECTORY ==="
-pwd
-
-echo "=== LIST CURRENT DIRECTORY ==="
-ls -la
-
-echo "=== TREE (2 levels) ==="
-find . -maxdepth 2 -type f
-
-echo "=== CODEDEPLOY ENV VARS ==="
-env | sort
 
 IMAGE=$(cat image_tag.txt)
+echo "Found image >>> "
+echo $IMAGE
+
+# Read image tag
+IMAGE=$(cat "$IMAGE_FILE")
 REGISTRY=$(echo "$IMAGE" | awk -F/ '{print $1}')
 
+# Log in to ECR
 aws ecr get-login-password --region us-east-1 \
-| docker login --username AWS --password-stdin "$REGISTRY"
+  | docker login --username AWS --password-stdin "$REGISTRY"
 
-ACTIVE_IMAGE=$(cat /home/ec2-user/image_tag.txt)
-
-docker pull $ACTIVE_IMAGE
-docker run -d -p 3000:3000 --name app -e PORT=3000 $ACTIVE_IMAGE
+# Pull and run container
+docker pull "$IMAGE"
+docker run -d -p 3000:3000 --name app -e PORT=3000 "$IMAGE"
